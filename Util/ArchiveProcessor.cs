5d4e61b57c3fa19470bdd832c360f6de79c70a36
ゆろち <1llum1n4t1@duck.com>
2026-01-14 17:15:49 +0900

    Fix: Change compression format name from LHA to LZH
    - Change SupportedCompressionFormats in Settings.cs from 'LHA' to 'LZH'
    - Update supportedFormats check in ArchiveProcessor.cs from 'lha' to 'lzh'
    - LZH is the correct and standard name for the LHA compression format
    - This ensures UI consistency with the actual file extension (.lzh)

--- Util/ArchiveProcessor.cs
+++ Util/ArchiveProcessor.cs
@@ -269,7 +269,7 @@ public static class ArchiveProcessor
             }
 
             // 圧縮形式の確認
-            var supportedFormats = new[] { "zip", "7z", "tar", "gz", "bz2", "xz", "cab", "wim", "lha" };
+            var supportedFormats = new[] { "zip", "7z", "tar", "gz", "bz2", "xz", "cab", "wim", "lzh" };
             if (!supportedFormats.Contains(format.ToLowerInvariant()))
             {
                 Logger.Log($"サポートされていない圧縮形式です: {format}");
